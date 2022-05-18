30b97f4458462c93a9461a1fbd36d651cfb73b00
LucasJeronimo <ljstudio@gmail.com>
2022-05-18 18:49:34 -0300

    Forgot password test configuration with EtherealMain

---
+++ src/config/mail/EtherealMail.ts
@@ -0,0 +1,60 @@
+import nodemailer from 'nodemailer';
+import HandlebarsMailTemplate from './HandlebarsMailTemplate';
+
+interface ITemplateVariable {
+   [key: string]: string | number;
+}
+
+interface IParseMailTemplate {
+   file: string;
+   variables: ITemplateVariable;
+}
+
+interface IMailContact {
+   name: string;
+   email: string;
+}
+
+interface ISendMail {
+   to: IMailContact;
+   from?: IMailContact;
+   subject: string;
+   templateData: IParseMailTemplate;
+}
+
+export default class EtherealMail {
+   static async sendMail({
+      to,
+      from,
+      subject,
+      templateData,
+   }: ISendMail): Promise<void> {
+      const account = await nodemailer.createTestAccount();
+      const transporter = nodemailer.createTransport({
+         host: account.smtp.host,
+         port: account.smtp.port,
+         secure: account.smtp.secure,
+         auth: {
+            user: account.user,
+            pass: account.pass,
+         },
+      });
+
+      const mailTemplate = new HandlebarsMailTemplate();
+      const message = await transporter.sendMail({
+         from: {
+            name: from?.name || 'Sales API Team',
+            address: from?.email || 'sales_team@apivendas.com',
+         },
+         to: {
+            name: to.name,
+            address: to.email,
+         },
+         subject: subject,
+         html: await mailTemplate.parse(templateData),
+      });
+
+      console.log('Message sent: %s', message.messageId);
+      console.log('Preview URL: %s', nodemailer.getTestMessageUrl(message));
+   }
+}
--- src/modules/users/services/SendForgotPasswordEmailService.ts
+++ src/modules/users/services/SendForgotPasswordEmailService.ts
@@ -1,7 +1,9 @@
 import AppError from '@shared/errors/AppError';
 import { getCustomRepository } from 'typeorm';
+import path from 'path';
 import UsersRepository from '../typeorm/repositories/UsersRepository';
 import UserTokensRepository from '../typeorm/repositories/UserTokensRepository';
+import EtherealMail from '@config/mail/EtherealMail';
 
 interface IRequest {
    email: string;
@@ -18,9 +20,26 @@ class SendForgotPasswordEmailService {
          throw new AppError('User not found.');
       }
 
-      const token = await userTokensRepository.generate(user.id);
+      const { token } = await userTokensRepository.generate(user.id);
 
-      console.log(token);
+      const forgotPasswordTemplate = path.resolve(
+         __dirname,
+         '..',
+         'views',
+         'forgot_password.hbs',
+      );
+
+      await EtherealMail.sendMail({
+         to: { name: user.name, email: user.email },
+         subject: '[Sales API] Password recovery',
+         templateData: {
+            file: forgotPasswordTemplate,
+            variables: {
+               name: user.name,
+               link: `http://localhost:3333/reset_password?token=${token}`,
+            },
+         },
+      });
    }
 }
 
